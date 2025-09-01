0a904fcd1d49ba1dee79631e45fef967af0b7b24
Carlos González <carlos85g@gmail.com>
2025-08-31 18:59:01 -0600

    fix(EnsureActionAllowed): Log exception messages

--- app/Http/Middleware/EnsureActionAllowed.php
+++ app/Http/Middleware/EnsureActionAllowed.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use App\Helpers\RuleEvaluator;
 
@@ -17,12 +18,18 @@ class EnsureActionAllowed
     public function handle(Request $request, Closure $next, string $action): Response
     {
         /** @var bool */
-        $isAllowed = app()
+        $isAllowed = false;
+
+        try {
+            $isAllowed = app()
                     ->make(RuleEvaluator::class)
                     ->isUserAllowed(
                         $action,
                         $request->user()
                     );
+        }catch(\Exception $e) {
+            Log::warning($e->getMessage());
+        }
 
         if(!$isAllowed) {
             return response()->json([
