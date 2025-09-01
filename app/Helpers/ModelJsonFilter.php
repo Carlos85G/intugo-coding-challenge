fbd0d92535e223274d57a8a3e5c1eeff9d6a4f13
Carlos González <carlos85g@gmail.com>
2025-09-01 02:37:51 -0600

    feat(Nested Filter): Model JSON Filter class

---
+++ app/Helpers/ModelJsonFilter.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use App\Exceptions\ModelJsonFilter\InvalidClassException;
+use App\Exceptions\ModelJsonFilter\InvalidJsonException;
+/**
+ * Helper class to create model query builders from JSON strings
+ * 
+ * @author Carlos González
+ */
+class ModelJsonFilter {
+    /** @var string Separator for nested relationships */
+    protected static string $separator = '.';
+
+    /**
+     * Public function to create a query builder from a JSON string and a model classname.
+     * 
+     * @param   string    $json   The filters to create in the builder.
+     * @param   string    $class  The class name of the model to create the builder.
+     * @return  Builder           The filtered builder.
+     * @throws  Exception
+     */
+    public static function makeBuilderFromJson(string $json, string $class) : Builder
+    {
+        /** @var mixed[] */
+        $filters = self::parseJson($json);
+
+        /** @var Builder The query builder */
+        $query = self::generateQuery($class);
+
+        self::addWhereClausesByFilters($query, $filters);
+
+        return $query;
+    }
+
+    /**
+     * Protected function to get a tag from the model in the builder.
+     * 
+     * @param   Builder   $query    The builder.
+     * @return  string              The tag.
+     */
+    protected static function getModelClassTag(Builder $query) : string
+    {
+        $class = get_class($query->getModel());
+
+        return strtolower(
+            class_basename($class)
+        );
+    }
+
+    /**
+     * Protected function to generate a builder from a model class name.
+     * 
+     * @param   string                  $class  The model class name.
+     * @return  Builder                         The builder.
+     * @throws  InvalidClassException
+     */
+    protected static function generateQuery(string $class) : Builder
+    {
+        if (!is_a($class, Model::class, true)) {
+            throw new InvalidClassException('Invalid class - Does not correspond to a model');
+        }
+
+        return call_user_func([
+            "{$class}",
+            "query"
+        ]);
+    }
+
+    /**
+     * Protected function to add where clauses and groups of where clauses,
+     * according to filter definitions.
+     * 
+     * @param   Builder   $query    The builder to add the clauses to, passed by reference.
+     * @param   mixed[]   $filters  The filters. Keys are foreign and local property names,
+     *                              values are the data to check against.
+     * @return  void
+     */
+    protected static function addWhereClausesByFilters(Builder &$query, array $filters) : void
+    {
+        [$filterKeys, $fieldData] = Arr::divide($filters);
+
+        foreach ($filterKeys as $index => $filterKey) {
+            [
+                /**
+                 * Model relationship chain.
+                 * 
+                 * Empty means local model property
+                 * 
+                 * @var string
+                 * */
+                $relationship,
+                /** @var string Field name at destination model */
+                $field
+            ] = self::getRelationshipAndFieldFromKey($filterKey, self::getModelClassTag($query));
+
+            /** @var mixed Data to look for */
+            $value = $fieldData[$index];
+
+            /**
+             * Check if there are multiple values to look for
+             * in the same property.
+             * 
+             * @var bool
+             */
+            $isValueArray = is_array($value);
+
+            /** @var bool Check if there's a relationship */
+            $isRelationship = !empty($relationship);
+
+            if ($isRelationship && $isValueArray) {
+                /* Relationship property with multiple values */
+                self::addNestedOrWhereHas($query, $relationship, $field, $value);
+            } else if ($isValueArray) {
+                /* Local property with multiple values */
+                self::addNestedOrWhere($query, $field, $value);
+            } else if ($isRelationship) {
+                /* Relationship property with single value */
+                self::addWhereHas($query, $relationship, $field, $value);
+            } else {
+                /* Local property with single value */
+                self::addWhere($query, $field, $value);
+            }
+        }
+    }
+
+    /**
+     * Protected function to parse JSON filters for the builder
+     * 
+     * @param   string              $json   The filter text
+     * @return  mixed[]                     The parsed filter data
+     * @throws  InvalidJsonException
+     */
+    protected static function parseJson(string $json) : array
+    {
+        $data = json_decode($json, true);
+
+        if (is_null($data)) {
+            throw new InvalidJsonException("Invalid JSON filter");
+        }
+
+        return $data;
+    }
+
+    /**
+     * Protected function to separate relationships and target property names
+     * 
+     * @param   string    $key  The filter relationship key
+     * @param   string    $tag  The model class name tag, to identify local class
+     * @return  string[]        The relationship path, in dot syntax,
+     *                          and the target property name
+     */
+    protected static function getRelationshipAndFieldFromKey(string $key, string $tag) : array
+    {
+        /** @var string[] */
+        $chunks = explode(self::$separator, $key);
+
+        /** @var string */
+        $field = array_pop($chunks);
+
+        if (!empty($chunks) && strtolower($chunks[0]) == $tag) {
+            /* First as class ID defaults to current builder, so it's removed */
+            array_shift($chunks);
+        }
+
+        /** @var string */
+        $relationship = implode(self::$separator, $chunks);
+
+        return [$relationship, $field];
+    }
+
+    /**
+     * Protected function to add a where clause to the builder
+     * 
+     * @param   Builder $query    The query.
+     * @param   string  $field    The property to compare.
+     * @param   string  $value    The property value to compare against.
+     * @return  void
+     */
+    protected static function addWhere(Builder &$query, string $field, string $value) : void
+    {
+        $query->where($field, $value);
+    }
+
+    /**
+     * Protected function to add an orWhere clause to the builder
+     * 
+     * @param   Builder $query    The query.
+     * @param   string  $field    The property to compare.
+     * @param   string  $value    The property value to compare against.
+     * @return  void
+     */
+    protected static function addOrWhere(Builder &$query, string $field, string $value) : void
+    {
+        $query->orWhere($field, $value);
+    }
+
+    /**
+     * Protected function to add a whereHas clause to the builder
+     * 
+     * @param   Builder $query    The query.
+     * @param   string  $field    The property to compare.
+     * @param   string  $value    The property value to compare against.
+     * @return  void
+     */
+    protected static function addWhereHas(Builder &$query, string $relationship, string $field, string $value) : void
+    {
+        $query->WhereHas($relationship, function ($query) use ($field, $value) : void {
+            self::addWhere($query, $field, $value);
+        });
+    }
+
+    /**
+     * Protected function to add an orWhereHas clause to the builder
+     * 
+     * @param   Builder $query    The query.
+     * @param   string  $field    The property to compare.
+     * @param   string  $value    The property value to compare against.
+     * @return  void
+     */
+    protected static function addOrWhereHas(Builder &$query, string $relationship, string $field, string $value) : void
+    {
+        $query->orWhereHas($relationship, function ($query) use ($field, $value) : void {
+            self::addWhere($query, $field, $value);
+        });
+    }
+
+    /**
+     * Protected function to add a group of nested orWhereHas clauses to the builder
+     * 
+     * @param   Builder $query          The query.
+     * @param   string  $relationship   The property to compare.
+     * @param   string  $field          The property to compare.
+     * @param   string  $values         The property values to compare against.
+     * @return  void
+     */
+    protected static function addNestedOrWhereHas(Builder &$query, string $relationship, string $field, array $values) : void
+    {
+        $query->where(function ($query) use ($relationship, $field, $values) : void {
+            $firstWhere = true;
+
+            foreach ($values as $value) {
+                self::{($firstWhere) ? "addWhereHas" : "addOrWhereHas"}($query, $relationship, $field, $value);
+
+                if ($firstWhere) {
+                    $firstWhere = false;
+                }
+            }
+        });
+    }
+
+    /**
+     * Protected function to add a group of nested orWhere clauses to the builder
+     * 
+     * @param   Builder $query          The query.
+     * @param   string  $field          The property to compare.
+     * @param   string  $values         The property values to compare against.
+     * @return  void
+     */
+    protected static function addNestedOrWhere(Builder &$query, string $field, array $values) : void
+    {
+        $query->where(function ($query) use ($field, $values) : void {
+            $firstWhere = true;
+
+            foreach ($values as $value) {
+                self::{($firstWhere) ? "addWhere" : "addOrWhere"}($query, $field, $value);
+
+                if ($firstWhere) {
+                    $firstWhere = false;
+                }
+            }
+        });
+    }
+}
